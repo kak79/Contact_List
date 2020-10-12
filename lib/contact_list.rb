a0f4475becc5250284c8ddb8032f93e820b9984e
kak79 <kohelka@gmail.com>
2020-10-12 12:12:26 -0500

    added phone number array and contact name hash

--- lib/contact_list.rb
+++ lib/contact_list.rb
@@ -1,4 +1,16 @@
 class Contact_List
 
-    
+  
+  
+  def self.contact_name_hash
+    contact_name.each do |first_name, last_name|
+      self.send("#{first_name}=", last_name)
+    end
+  end
+
+  def self.phone_number_array
+    phone_number = []
+  end
+
+
 end
