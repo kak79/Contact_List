9d7725b08636b8a6cdb49392a193731a34a4fb23
kak79 <kohelka@gmail.com>
2020-10-12 11:43:21 -0500

    too many changes to list

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,5 +1,29 @@
 class Interface
+  attr_accessor :input
 
+  def initialize
+      @input = ""
+  end
+  
+  def start
+    "Welcome to the Contact List"
+    menu
+  end
 
+  def input
+    self.input = gets.chomp 
+  end
+
+  def menu
+    "Do you want to add a new contact?"
+    input
+    "What is the first name of the contact?"
+    input
+    "What is the last name of the contact?"
+    input
     
+  end
+ 
+  
+  
 end
